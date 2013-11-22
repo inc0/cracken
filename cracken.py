7f8eefd9477e586419a666f1f440689124ce01bf
Michal Jastrzebski <michal.jastrzebski@allegro.pl>
2013-11-22 15:07:29 +0100

    work in progress

--- cracken.py
+++ cracken.py
@@ -1,13 +1,17 @@
-from io import StringIO
+import sys
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from urllib import request
+
+
+dest_url = sys.argv[1]
 
 
 class CrackenHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        out = StringIO("foo")
+        resp = request.urlopen(dest_url + self.path)
         self.send_response(200)
-        self.wfile.write(out)
+        self.wfile.write(bytes(resp.read().decode('UTF-8'), "UTF-8"))
         return
 
 
